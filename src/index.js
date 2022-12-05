4fb1a7e6ac901b15f8511cb9d31423cc3a057e98
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-05 17:22:24 -0500

    Add Full Stack TakeHome

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bpk-stylesheets';
+import 'bpk-stylesheets/font';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+/* Create functionality in a sensible file to do the following:
+ * TODO - retrieve the results from the provided flight.json
+ * TODO - a better format for displaying results to the results page.
+ */
